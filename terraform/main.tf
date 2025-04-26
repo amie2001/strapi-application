8fd063e1f1fbb5ef92b69e0a498472f726d70db7
pavi2001 <74369749+amie2001@users.noreply.github.com>
2025-04-26 17:04:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,50 +3,46 @@ provider "aws" {
 }
 
 resource "aws_security_group" "strapi_sg_task4" {
-  name        = "strapi-sg_task4"
+  name        = "strapi-sg-task4"
   description = "Security group for Strapi EC2 instance"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (can be restricted for security)
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH (you can restrict later for security)
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP access (mapped to Strapi's 1337)
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP access (port 80)
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound
   }
 }
 
 resource "aws_instance" "strapi_task4" {
-  ami                    = "ami-0e35ddab05955cf57"  # Amazon Linux 2 AMI (Mumbai region)
-  instance_type          = "t2.medium"
-  key_name               = "strapi-using-docker"     # Your EC2 key pair name
+  ami                         = "ami-0e35ddab05955cf57" # Amazon Linux 2 AMI (Mumbai region)
+  instance_type               = "t2.medium"
+  key_name                    = "strapi-using-docker"
   associate_public_ip_address = true
   security_groups             = [aws_security_group.strapi_sg_task4.name]
+  iam_instance_profile        = "EC2ECRAccessRole" # Make sure the Role is attached
 
-
-  iam_instance_profile = "EC2ECRAccessRole"
-
-
- user_data = <<-EOF
+  user_data = <<-EOF
     #!/bin/bash
     apt-get update -y
     apt-get install -y docker.io unzip
     systemctl start docker
     usermod -aG docker ubuntu
 
-    # Install AWS CLI v2
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     sudo ./aws/install
@@ -54,10 +50,9 @@ resource "aws_instance" "strapi_task4" {
     # Login to Amazon ECR
     aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 864899875002.dkr.ecr.ap-south-1.amazonaws.com
 
-    # Pull the Strapi Docker image from ECR using the provided tag
+    # Pull and Run the Strapi Docker image
     docker pull ${var.ecr_repository}:${var.image_tag}
 
-    # Run the Strapi container
     docker run -d -p 80:1337 \
       -e APP_KEYS="toBeModified1,toBeModified2" \
       -e API_TOKEN_SALT="tobemodified" \
